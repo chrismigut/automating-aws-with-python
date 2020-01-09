bde81ace736d04a9e5c397a5b859f49e54ecd71c
Chris Migut <cmiguthere@yahoo.com>
2020-01-08 23:27:08 -0500

    Imported click, similar to argparse lib

---
+++ 01-webotron/webotron/webotron.py
@@ -0,0 +1,14 @@
+import boto3
+import click
+
+session = boto3.Session(profile_name='pythonAutomation')
+s3 = session.resource('s3')
+
+@click.command('list-buckets')
+def list_buckets():
+    "List all s3 buckets"
+    for bucket in s3.buckets.all():
+        print(bucket)
+
+if __name__ == '__main__':
+    list_buckets()
