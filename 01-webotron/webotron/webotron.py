32060b441d93263f4ebb363828eee14c967eb81d
Chris Migut <cmiguthere@yahoo.com>
2020-01-09 00:05:05 -0500

    Added more features to access s3 with click lib

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -4,11 +4,28 @@ import click
 session = boto3.Session(profile_name='pythonAutomation')
 s3 = session.resource('s3')
 
-@click.command('list-buckets')
+@click.group()
+def cli():
+    "Webotron deploys websites to AWS"
+    pass
+
+@cli.command('list-buckets')
 def list_buckets():
     "List all s3 buckets"
     for bucket in s3.buckets.all():
         print(bucket)
 
+@cli.command('list-bucket-objects')
+@click.argument('bucket', nargs=1)
+def list_bucket_objects(bucket):
+    "List objects in an s3 bucket"
+    for obj in s3.Bucket(bucket).objects.all():
+        print(obj)
+
 if __name__ == '__main__':
-    list_buckets()
+    cli()
+
+
+# Grouping commands under cli
+# click.group
+#   -> cli.command(list-buckets)
--- README.md
+++ README.md
@@ -7,6 +7,14 @@ Practice Automating AWS with Python
 * Registered TLS Cert with Route53 from Certificate Manager
 
 ## 01-webotron
+
 Webotron is a script that will sync a local directory
 to an s3 bucket, and optionally configure Route 53 and
 CloudFront as well.
+
+### Features
+
+Webotron currently has the following Features
+
+- List buckets
+- List objects in a bucket
