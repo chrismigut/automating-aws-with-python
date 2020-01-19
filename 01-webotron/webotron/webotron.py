fdde25c4b9b93f396fa05f0c25d0271a7db32b0a
Chris Migut <cmiguthere@yahoo.com>
2020-01-19 02:41:39 -0500

    updated the session configuration to allow the user to specify the session profile as an option.

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -19,13 +19,21 @@ import click
 
 from bucket import BucketManager
 
-session = boto3.Session(profile_name='pythonAutomation')
-bucket_manager = BucketManager(session)
-
+session = None
+bucket_manager = None
 
 @click.group()
-def cli():
+@click.option('--profile', default=None, help="Use a given AWS profile.")
+def cli(profile):
     """Webotron deploys websites to AWS."""
+    global session, bucket_manager
+    session_cfg = {}
+    if profile:
+        session_cfg['profile_name'] = profile
+    else:
+        session_cfg['profile_name'] = 'pythonAutomation'
+    session = boto3.Session(**session_cfg) # glob that will un wrap to fit func
+    bucket_manager = BucketManager(session)
 
 
 @cli.command('list-buckets')
@@ -68,3 +76,5 @@ if __name__ == '__main__':
 # Grouping commands under cli
 # click.group
 #   -> cli.command(list-buckets)
+
+# orginal profile was pythonAutomation
